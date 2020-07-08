75dc32a42b1ae6ce3df6813e9a18b73b72ad359e
OlgaLiV <9755609@gmail.com>
2020-07-08 12:25:17 +0300

    HW18

--- com/syntax/class18/Employee.java
+++ com/syntax/class18/Employee.java
@@ -1,25 +1,31 @@
 package com.syntax.class18;
 
 public class Employee {
-static String CEO = "Sumair";
-int eID;
-double salary;
+	static String CEO = "Sumair";
+	int eID;
+	double salary;
 
-void display() {
-	System.out.println("The employee ID is: " + eID + " salary is: " + salary + " and CEO is: " + CEO);
-}
+	void display() {
+		System.out.println("The employee ID is: " + eID + " salary is: " + salary + " and CEO is: " + CEO);
+	}
+	
+	static void sayHelloToCeo() {
+		System.out.println("Hi " + CEO);
+	}
 
-public static void main(String[] args) {
-	Employee obj1 = new Employee();
-	obj1.eID = 345567;
-	obj1.salary = 120000;
-	obj1.display();
-	
-	Employee obj2 = new Employee();
-	obj2.eID = 6654322;
-	obj2.salary = 150000;
-	obj2.display();
-	
-	
-}
+	public static void main(String[] args) {
+		Employee obj1 = new Employee();
+		obj1.eID = 345567;
+		obj1.salary = 120000;
+		obj1.display();
+
+		Employee obj2 = new Employee();
+		obj2.eID = 6654322;
+		obj2.salary = 150000;
+		obj2.display();
+		
+		//accessing static method
+		sayHelloToCeo();
+
+	}
 }
---
+++ com/syntax/class18/Students.java
@@ -0,0 +1,33 @@
+package com.syntax.class18;
+
+public class Students {
+	String studentName;
+	int studentID;
+	static int numberOfStudents = 3;
+
+	void allStudents() {
+		System.out.println("The name of student is " + studentName + " and student ID is: " + studentID
+				+ " and total numbers of student in class is: " + numberOfStudents);
+	}
+
+	public static void main(String[] args) {
+		Students obj1 = new Students();
+		obj1.studentName = "Olga";
+		obj1.studentID = 282;
+		obj1.allStudents();
+		System.out.println("*********************");
+
+		Students obj2 = new Students();
+		obj2.studentName = "Aidana";
+		obj2.studentID = 111;
+		obj2.allStudents();
+		System.out.println("*********************");
+
+		Students obj3 = new Students();
+		obj3.studentName = "Dinara";
+		obj3.studentID = 222;
+		obj3.allStudents();
+
+	}
+
+}
