a779727e752d844cb8172bbb77507950195f6790
OlgaLiV <9755609@gmail.com>
2020-07-10 02:56:23 +0300

    Static keyword

--- com/syntax/class18/Students.java
+++ com/syntax/class18/Students.java
@@ -3,7 +3,7 @@ package com.syntax.class18;
 public class Students {
 	String studentName;
 	int studentID;
-	static int numberOfStudents = 3;
+	static int numberOfStudents;
 
 	void allStudents() {
 		System.out.println("The name of student is " + studentName + " and student ID is: " + studentID
@@ -14,18 +14,21 @@ public class Students {
 		Students obj1 = new Students();
 		obj1.studentName = "Olga";
 		obj1.studentID = 282;
+		numberOfStudents++;
 		obj1.allStudents();
 		System.out.println("*********************");
 
 		Students obj2 = new Students();
 		obj2.studentName = "Aidana";
 		obj2.studentID = 111;
+		numberOfStudents++;
 		obj2.allStudents();
 		System.out.println("*********************");
 
 		Students obj3 = new Students();
 		obj3.studentName = "Dinara";
 		obj3.studentID = 222;
+		numberOfStudents++;
 		obj3.allStudents();
 
 	}
